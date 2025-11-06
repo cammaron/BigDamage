d899c006d6ed9c0eceb696451c864ecc0de7e12d
Cameron Duff <dragonfistlimitless@gmail.com>
2025-11-06 20:20:03 +1100

    Refactor out the DmgPop's ability to scale in size proportionate to distance to its own distinct behaviour for use in Arcanism, and fixing missing implementation of colour change enable/disable property

---
+++ ScaleWithDistance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace BigDamage
+{
+    public class ScaleWithDistance : MonoBehaviour
+    {
+		public float minDistance = 3f;
+
+		public float? maxScale;
+		public float? extraBaseScale;
+
+		public bool debugLog = false;
+
+		Vector3 originalScale;
+
+		protected void Awake()
+        {
+			originalScale = transform.localScale;
+        }
+
+		protected void OnDestroy()
+        {
+			transform.localScale = originalScale;
+        }
+
+		protected void Update()
+        {
+			float distanceFromCamera = Vector3.Distance(transform.position, GameData.CamControl.transform.position);
+			float distanceScaleMultiplier = Mathf.Max(1, ((distanceFromCamera - minDistance) / Main.distanceToDoubleSize.Value) * 2f);
+			float totalMulti = extraBaseScale.GetValueOrDefault(1) * distanceScaleMultiplier;
+			if (maxScale.HasValue) totalMulti = Mathf.Min(maxScale.Value, totalMulti);
+			transform.localScale = originalScale * totalMulti;
+			if (debugLog) Main.Log.LogInfo($"ScaleWithDistance: distFromCamera {distanceFromCamera}, distScaleMulti {distanceScaleMultiplier}, totalMulti {totalMulti}, newScale {transform.localScale.x}");
+		}
+    }
+}
