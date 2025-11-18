b32ca39a4a93e9265dd1c40be839f64cfef081ef
Cameron Duff <dragonfistlimitless@gmail.com>
2025-11-19 10:48:59 +1100

    New features: DmgPops fan out in a wider arc without random overlap, orient correctly relative to camera (vanilla bug fix), slightly different speed/animation, configurable delay before they disappear for more chance to read

--- Main.cs
+++ Main.cs
@@ -11,44 +11,51 @@ namespace BigDamage
     {
         public const string PLUGIN_GUID = "com.raventhe.erenshor.BigDamage";
         public const string PLUGIN_NAME = "BigDamage";
-        public const string PLUGIN_VERSION = "1.0.3";
+        public const string PLUGIN_VERSION = "1.0.4";
 
         internal static ConfigEntry<float> maxDistance;
         internal static ConfigEntry<float> baseSizeFactor;
         internal static ConfigEntry<float> distanceToDoubleSize;
         internal static ConfigEntry<bool> enableColourChange;
+        
+        internal static ConfigEntry<float> startSpeed;
+        internal static ConfigEntry<float> speedReduction;
+        internal static ConfigEntry<float> juicyShrinkFactor;
+        internal static ConfigEntry<float> delayBeforeShrinkingAway;
+
         internal static Color physColour;
         internal static Color magColour;
         internal static Color eleColour;
         internal static Color voidColour;
         internal static Color poisonColour;
+        internal static Color critColour;
+
         internal static ManualLogSource Log;
-
-        private const float DEFAULT_MAX_DISTANCE = 55f;
-        private const float DEFAULT_SIZE_FACTOR = 2.5f;
-        private const float DEFAULT_DISTANCE_TO_DOUBLE_SIZE = 17f;
-
-        private const string DEFAULT_COLOUR_PHYSICAL = "#8c8c8c";
-        private const string DEFAULT_COLOUR_MAGIC = "#b500a6";
-        private const string DEFAULT_COLOUR_ELEMENTAL = "#54ffeb";
-        private const string DEFAULT_COLOUR_VOID = "#35026b";
-        private const string DEFAULT_COLOUR_POISON = "#8cff50";
 
         public void Awake()
         {
             Log = base.Logger;
+            string general = "0_General";
+            string colours = "1_Damage_Colours";
+            string dynamics = "2_Dynamics";
 
             Logger.LogInfo("Initialising.");
-            maxDistance = Config.Bind("General", "maxDistance", DEFAULT_MAX_DISTANCE, $"The distance a damage popup can be at before being hidden from the player. Higher = view hits further away. Vanilla default is 20. Mod default is {DEFAULT_MAX_DISTANCE}.");
-            baseSizeFactor = Config.Bind("General", "baseSizeFactor", DEFAULT_SIZE_FACTOR, $"An initial multiplier to damage popup font size. Just makin' it bigger regardless.. Mod default is {DEFAULT_SIZE_FACTOR}.");
-            distanceToDoubleSize = Config.Bind("General", "distanceToDoubleSize", DEFAULT_DISTANCE_TO_DOUBLE_SIZE, $"Every X units of distance, the damage popup text scale is doubled. Scaling over distance is the main purpose of this mod, as damage occurring further away needs to be larger in world space to be visible. Mod default is {DEFAULT_DISTANCE_TO_DOUBLE_SIZE}.");
-            enableColourChange = Config.Bind("General", "enableColourChange", true, $"{PLUGIN_NAME} attempts to increase damage readability by removing the colour gradient and choosing simple, bold colours. Set this to false if you prefer the original style.");
+            maxDistance = Config.Bind(general, "maxDistance", 55f, $"The distance a damage popup can be at before being hidden from the player. Higher = view hits further away. Vanilla default is 20.");
+            baseSizeFactor = Config.Bind(general, "baseSizeFactor", 2.5f, $"An initial multiplier to damage popup font size. Just makin' it bigger regardless.");
+            distanceToDoubleSize = Config.Bind(general, "distanceToDoubleSize", 17f, $"Every X units of distance, the damage popup text scale is doubled. Scaling over distance is the main purpose of this mod, as damage occurring further away needs to be larger in world space to be visible.");
+            enableColourChange = Config.Bind(general, "enableColourChange", true, $"{PLUGIN_NAME} attempts to increase damage readability by removing the colour gradient and choosing simple, bold colours. Set this to false if you prefer the original style.");
 
-            ColorUtility.TryParseHtmlString(Config.Bind("Damage_Colours", "physical", DEFAULT_COLOUR_PHYSICAL, $"Text colour for PHYSICAL damage. Default ${DEFAULT_COLOUR_PHYSICAL} (medium grey)").Value, out physColour);
-            ColorUtility.TryParseHtmlString(Config.Bind("Damage_Colours", "magical", DEFAULT_COLOUR_MAGIC, $"Text colour for MAGIC damage. Default ${DEFAULT_COLOUR_MAGIC} (light magenta)").Value, out magColour);
-            ColorUtility.TryParseHtmlString(Config.Bind("Damage_Colours", "elemental", DEFAULT_COLOUR_ELEMENTAL, $"Text colour for ELEMENTAL damage. Default ${DEFAULT_COLOUR_ELEMENTAL} (sky blue)").Value, out eleColour);
-            ColorUtility.TryParseHtmlString(Config.Bind("Damage_Colours", "void", DEFAULT_COLOUR_VOID, $"Text colour for VOID damage. Default ${DEFAULT_COLOUR_VOID} (deep purple)").Value, out voidColour);
-            ColorUtility.TryParseHtmlString(Config.Bind("Damage_Colours", "poison", DEFAULT_COLOUR_POISON, $"Text colour for POISON damage. Default ${DEFAULT_COLOUR_POISON} (acid green)").Value, out poisonColour);
+            ColorUtility.TryParseHtmlString(Config.Bind(colours, "physical", "#8c8c8c", $"Text colour for PHYSICAL damage. Default is medium grey.").Value, out physColour);
+            ColorUtility.TryParseHtmlString(Config.Bind(colours, "magical", "#b500a6", $"Text colour for MAGIC damage. Default is light magenta.").Value, out magColour);
+            ColorUtility.TryParseHtmlString(Config.Bind(colours, "elemental", "#54ffeb", $"Text colour for ELEMENTAL damage. Default is sky blue.").Value, out eleColour);
+            ColorUtility.TryParseHtmlString(Config.Bind(colours, "void", "#35026b", $"Text colour for VOID damage. Default is deep purple.").Value, out voidColour);
+            ColorUtility.TryParseHtmlString(Config.Bind(colours, "poison", "#8cff50", $"Text colour for POISON damage. Default is acid green.").Value, out poisonColour);
+            ColorUtility.TryParseHtmlString(Config.Bind(colours, "crit", "#ffe300", $"Text colour for critical hits (regardless of damage type). I'm gonna call the default 'banana yellow.'").Value, out critColour);
+
+            startSpeed  = Config.Bind(dynamics, "startSpeed", 16f, $"Initial fly-out speed of DmgPop. BigDamage stylistically sets a higher base speed with a sharper speed drop-off (via speedReduction setting). For vanilla, set this to 3.");
+            speedReduction = Config.Bind(dynamics, "speedReduction", 18f, $"DmgPop speed reduces by this extra amount per second (on top of vanilla). For vanilla, set this to 0.");
+            juicyShrinkFactor = Config.Bind(dynamics, "juicyShrinkFactor", .5f, $"DmgPops spawn at full size and will immediately begin progressively shrinking to this size factor. Seems to be for juiciness. Vanilla is different across different damage types. Set to 1 if you don't want any juicy shrink.");
+            delayBeforeShrinkingAway = Config.Bind(dynamics, "delayBeforeShrinkingAway", .5f, $"An extra period of gentle stillness (after fly-out finishes) before we begin fading away, to ensure you get to grok those numbers with those big meaty eyeballs of yours.");
 
             var harmony = new Harmony(PLUGIN_GUID);
             harmony.PatchAll();
--- Patches/DmgPop.cs
+++ Patches/DmgPop.cs
@@ -1,13 +1,45 @@
 ﻿using UnityEngine;
-using BepInEx;
+using System.Collections.Generic;
 using HarmonyLib;
 
 namespace BigDamage.Patches
 {
+	[HarmonyPatch(typeof(DmgPop), "Update")]
+	public class DmgPop_Update {
+
+		static readonly Dictionary<DmgPop, float> beginShrinkTimeByDmgPop = new Dictionary<DmgPop, float>();
+
+		static bool Prefix(DmgPop __instance)
+        {
+			__instance.speed -= Time.deltaTime * Main.speedReduction.Value; // Starting with a higher initial speed, but decreasing speed faster, too
+			if (__instance.speed <= 0f)
+            {
+				__instance.speed = 0f;
+				if (!beginShrinkTimeByDmgPop.TryGetValue(__instance, out float beginShrinkTime))
+                {
+					beginShrinkTime = Time.realtimeSinceStartup + Main.delayBeforeShrinkingAway.Value;
+					beginShrinkTimeByDmgPop[__instance] = beginShrinkTime;
+				}
+				
+				if (Time.realtimeSinceStartup < beginShrinkTime) // Don't let the original code run until we're ready to shrink and disappear
+					return false;
+            }
+
+			return true;
+		}
+		static void Postfix(DmgPop __instance, float ___dest)
+        {
+			if (___dest <= 0f)
+				beginShrinkTimeByDmgPop.Remove(__instance);
+		}
+	}
 	[HarmonyPatch(typeof(DmgPop), nameof(DmgPop.LoadInfo), new[] { typeof(int), typeof(bool), typeof(GameData.DamageType), typeof(Transform) })]
     public class DmgPop_LoadInfo
     {
-		static void Postfix(DmgPop __instance, int _dmg, bool _crit, GameData.DamageType _type, Transform _tar)
+		const float SIDE_FACTOR_MAX = .55f;
+		static float sideFactor = -SIDE_FACTOR_MAX;
+
+		static void Postfix(DmgPop __instance, int _dmg, bool _crit, GameData.DamageType _type, Transform _tar, ref Vector3 ___moveDir, ref Vector3 ___sideDir, ref float ___fontDel, ref float ___destSize, ref float ___dest)
         {
 			if (__instance.GetComponent<ScaleWithDistance>() == null)
             {
@@ -20,30 +52,50 @@ namespace BigDamage.Patches
 			if (npc?.NamePlate != null)
 				__instance.transform.position = npc.NamePlate.position;
 
-			Traverse.Create(__instance).Field("sideDir").SetValue(new Vector3(Random.Range(-1f, 1f), 0, 0));
+			___sideDir = Vector3.zero;
+
+			// Vanilla attempts to make DmgPops float to the left or right (so as not to consistently overlap) based on a random factor applied to X direction.
+			// The problem is that since they spawn in world space, "X" is a completely arbitrary direction that doesn't necessarly correlate to the left or right of the screen view,
+			// which also means that depending on the relative position of the enemy you're fighting, those numbers may go "back" or forward rather than left or right.
+			// Addressing this by making their lateral movement relative to camera orientation, and also rather than a random amount of lateral movement,
+			// specifically fanning them out in a consistent arc shape so two don't overlap by chance
+			sideFactor += .2f;
+			___moveDir = (-GameData.CamControl.ActualCam.transform.right) * sideFactor + Vector3.up * .6f;
+			if (sideFactor >= SIDE_FACTOR_MAX * 1.1f) sideFactor = -SIDE_FACTOR_MAX;
+
+			__instance.speed = Main.startSpeed.Value;
+			__instance.Num.fontSize = 2.8f;
+			___fontDel = 0f;
+			___destSize = __instance.Num.fontSize * Main.juicyShrinkFactor.Value;
+			___dest = 1.2f * 60f;
 
 			if (Main.enableColourChange.Value)
             {
 				__instance.Num.enableVertexGradient = false;
-				switch (_type)
-				{
-					case GameData.DamageType.Physical:
-						__instance.Num.color = Main.physColour;
-						break;
-					case GameData.DamageType.Magic:
-						__instance.Num.color = Main.magColour;
-						return;
-					case GameData.DamageType.Elemental:
-						__instance.Num.color = Main.eleColour;
-						return;
-					case GameData.DamageType.Void:
-						__instance.Num.color = Main.voidColour;
-						return;
-					case GameData.DamageType.Poison:
-						__instance.Num.color = Main.poisonColour;
-						return;
-					default:
-						return;
+				if (_crit) 
+					__instance.Num.color = Main.critColour;
+				else
+                {
+					switch (_type)
+					{
+						case GameData.DamageType.Physical:
+							__instance.Num.color = Main.physColour;
+							break;
+						case GameData.DamageType.Magic:
+							__instance.Num.color = Main.magColour;
+							return;
+						case GameData.DamageType.Elemental:
+							__instance.Num.color = Main.eleColour;
+							return;
+						case GameData.DamageType.Void:
+							__instance.Num.color = Main.voidColour;
+							return;
+						case GameData.DamageType.Poison:
+							__instance.Num.color = Main.poisonColour;
+							return;
+						default:
+							return;
+					}
 				}
 			}
 		}
